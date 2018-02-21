c5ff38d362534337f9dc379c6487e04e7a7128b5
fatima <fatima.elyousfi83@gmail.com>
2018-02-21 09:47:30 +0100

    first master

---
+++ fatimaSpringBoot/src/main/java/dev/fatimaSpringBoot/FatimaSpringBootApplication.java
@@ -0,0 +1,12 @@
+package dev.fatimaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatimaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatimaSpringBootApplication.class, args);
+	}
+}
